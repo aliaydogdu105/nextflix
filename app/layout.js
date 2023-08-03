29f3fd6603461f720b15b17eb980927c354cebb6
aliaydogdu105 <aliaydogdu105@gmail.com>
2023-08-03 18:36:23 +0300

    feat: add global-reset css

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,16 @@
+import { Inter } from "@next/font/google";
+
+import "../styles/reset.css";
 import "../styles/global.css";
+
+const interFontFamily = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={interFontFamily.className}>
+      <body className=" container bg-black text-base text-white">
+        {children}
+      </body>
     </html>
   );
 }
